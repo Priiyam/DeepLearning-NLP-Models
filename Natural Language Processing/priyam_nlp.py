7d74c0afca4894d6c0f42145840f53456fc4ed4f
priiyam <crifte@gmail.com>
2018-03-11 14:21:31 +0530

    NLP added

---
+++ Natural Language Processing/priyam_nlp.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import nltk
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+
+dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter="\t", quoting=3)
+
+
+
+    
+    
+    
+    
+
