9c9714b0fd17e75ccc8b8c3d79cf7d32d2cbb639
priiyam <crifte@gmail.com>
2018-03-12 14:22:50 +0530

    NLP added

--- Natural Language Processing/priyam_nlp.py
+++ Natural Language Processing/priyam_nlp.py
@@ -19,6 +19,14 @@ for i in range(1000):
     review = " ".join(review)
     corpus.append(review)
     
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=0)
+
 
 
 
